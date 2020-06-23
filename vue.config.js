4cfcbd265100067c9ef43f1e2706b6bfb2520188
elonar <magmacarrier>
2020-06-23 17:12:17 +0800

    0.3 git-actions pre

---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "transpileDependencies": [
+    "vuetify"
+  ],
+  outputDir: 'dist', publicPath: process.env.NODE_ENV === 'production' ? '/d3Trial/' : '/',
+  productionSourceMap:false
+}
